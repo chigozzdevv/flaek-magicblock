df28e6bde4976a87d9ef5d61072c1ae4229e5ead
Ukpaa Chigozie <chigozzdev@gmail.com>
2026-02-01 09:53:57 -0800

    refactor: remove stale files

--- flaek-server/src/features/attestations/attestation.validators.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const verifyAttestationSchema = z.object({
-  body: z.object({
-    pipeline_hash: z.string(),
-    attestation: z.any(),
-  }),
-});
--- flaek-server/src/features/datasets/dataset.types.ts
+++
@@ -1,2 +0,0 @@
-export type DatasetId = string;
-
